fa0d4f299acc7606425ed64693e14f0ad234743b
ritika2707 <108430007+ritika2707@users.noreply.github.com>
2024-08-01 19:49:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,20 +9,9 @@ const app = express();
 dbConnect();
 
 app.use(express.json());
-
-const corsOptions = {
-    origin: 'https://rating-app-frontend.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api", audiobookRoutes);
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: true, message: 'Internal Server Error' });
-});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
